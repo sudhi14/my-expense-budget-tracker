05a20c58d6e23b93a0c491f73d666b19bb3f8cd4
Sudheep Somasundaram <ssomasundaram@securityscorecard.io>
2024-02-13 18:27:12 -0500

    Adding sidebar styles

--- src/components/GlobalSidebar/index.tsx
+++ src/components/GlobalSidebar/index.tsx
@@ -1,40 +1,42 @@
 import { NavLink } from "react-router-dom";
-import {
-  Dashboard,
-  AccountBalance,
-  Settings,
-  Logout,
-} from "@mui/icons-material";
 import GlobalSidebarItems from "../GlobalSidebarItems";
+import {
+  MdOutlineSpaceDashboard,
+  MdOutlineSettings,
+  MdOutlineLogout,
+} from "react-icons/md";
+import { FaSearchDollar } from "react-icons/fa";
 
 const GlobalSidebar = () => {
   const navItems = [
     {
       title: "Dashboard",
       link: "/home/dashboard",
-      icon: <Dashboard />,
+      icon: <MdOutlineSpaceDashboard size={25} />,
     },
     {
       title: "Finances",
       link: "/home/finances",
-      icon: <AccountBalance />,
+      icon: <FaSearchDollar size={25}  />,
     },
     {
       title: "Settings",
       link: "/home/settings",
-      icon: <Settings />,
+      icon: <MdOutlineSettings size={25} />,
     },
   ];
 
   return (
-    <div className="fixed flex flex-col items-center w-64 h-screen bg-emerald-900 ">
-      <div className="flex flex-col items-start h-screen py-10">
+    <div className="fixed flex flex-col items-center w-64 h-screen shadow-lg">
+      <div className="flex flex-col justify-center items-center h-screen w-full py-10">
         {navItems.map((curr) => (
           <GlobalSidebarItems {...curr} />
         ))}
         <div className="mb-10 mt-auto flex gap-5">
-          <Logout />
-          <NavLink to="/">Logout</NavLink>
+          <MdOutlineLogout size={25} className="text-emerald-800" />
+          <NavLink to="/" className="text-emerald-800 w-20">
+            Logout
+          </NavLink>
         </div>
       </div>
     </div>
--- src/components/GlobalSidebarItems/index.tsx
+++ src/components/GlobalSidebarItems/index.tsx
@@ -1,12 +1,15 @@
+import { IconContext } from "react-icons";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 
 const GlobalSidebarItems = (curr) => {
   return (
-    <div>
-      <div className="mb-10 flex gap-5">
-        {curr.icon}
-        <NavLink to={curr.link}>{curr.title}</NavLink>
-      </div>
+    <div className="flex flex-row gap-5 w-full mb-10 hover:cursor-pointer py-2 group hover:bg-[#1dae7e]">
+      <div className=" w-2 rounded-sm bg-emerald-800 ml-1 mr-5 scale-0 group-hover:scale-100 delay-100 ease-in-out duration-300"></div>
+      <div className="text-emerald-800">{curr.icon}</div>
+      <NavLink to={curr.link} className="text-emerald-800 w-20">
+        {curr.title}
+      </NavLink>
     </div>
   );
 };
