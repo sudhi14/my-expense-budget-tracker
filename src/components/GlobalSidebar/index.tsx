ae36d0532034827f3aa1eefb1c586fcc26cffe00
Sudheep Somasundaram <sudheep.1426@gmail.com>
2024-02-14 11:06:32 -0500

    Adding more side bar design

--- src/components/GlobalSidebar/index.tsx
+++ src/components/GlobalSidebar/index.tsx
@@ -1,48 +1,50 @@
-import { NavLink } from "react-router-dom";
-import GlobalSidebarItems from "../GlobalSidebarItems";
+import { NavLink } from 'react-router-dom'
+import GlobalSidebarItems from '../GlobalSidebarItems'
 import {
-  MdOutlineSpaceDashboard,
-  MdSpaceDashboard,
-  MdOutlineSettings,
-  MdOutlineLogout,
-} from "react-icons/md";
-import { FaSearchDollar } from "react-icons/fa";
-import ExpenseTrackerLogo from "../../assets/images/login/expense-tracker-logo.png";
+    MdOutlineSpaceDashboard,
+    MdSpaceDashboard,
+    MdOutlineSettings,
+    MdOutlineLogout,
+} from 'react-icons/md'
+import { FaSearchDollar } from 'react-icons/fa'
 
 const GlobalSidebar = () => {
-  const navItems = [
-    {
-      title: "Dashboard",
-      link: "/home/dashboard",
-      icon: <MdSpaceDashboard size={25} />,
-    },
-    {
-      title: "Finances",
-      link: "/home/finances",
-      icon: <FaSearchDollar size={25} />,
-    },
-    {
-      title: "Settings",
-      link: "/home/settings",
-      icon: <MdOutlineSettings size={25} />,
-    },
-  ];
+    const navItems = [
+        {
+            title: 'Dashboard',
+            link: '/home/dashboard',
+            icon: <MdOutlineSpaceDashboard size={25} />,
+        },
+        {
+            title: 'Finances',
+            link: '/home/finances',
+            icon: <FaSearchDollar size={25} />,
+        },
+        {
+            title: 'Settings',
+            link: '/home/settings',
+            icon: <MdOutlineSettings size={25} />,
+        },
+        {
+            title: 'Logout',
+            link: '/',
+            icon: <MdOutlineLogout size={25} />,
+        },
+    ]
 
-  return (
-    <div className="fixed flex flex-col w-64 h-screen shadow-lg bg-white">
-      <section className="flex flex-col justify-center items-center h-screen w-full py-10">
-        {navItems.map((curr) => (
-          <GlobalSidebarItems {...curr} />
-        ))}
-        <div className="mb-10 mt-auto flex gap-5">
-          <MdOutlineLogout size={25} className="text-emerald-800" />
-          <NavLink to="/" className="text-emerald-800 w-20">
-            Logout
-          </NavLink>
+    return (
+        <div className="fixed inset-0 flex flex-col w-64 h-screen shadow-lg bg-gray-50 ">
+            <section className="flex flex-col h-screen w-full">
+                {navItems.map((curr) => (
+                    <GlobalSidebarItems {...curr} />
+                ))}
+                <div className="mb-10 mt-auto flex gap-5">
+                    <MdOutlineLogout size={25} />
+                    <NavLink to="/">Logout</NavLink>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  );
-};
+    )
+}
 
-export default GlobalSidebar;
+export default GlobalSidebar
--- src/components/GlobalSidebarItems/index.tsx
+++ src/components/GlobalSidebarItems/index.tsx
@@ -1,13 +1,17 @@
 import { IconContext } from "react-icons";
-import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { MdOutlineLogout, MdOutlineSpaceDashboard } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 
 const GlobalSidebarItems = (curr) => {
   return (
-    <div className="flex flex-row gap-5 w-full mb-10 hover:cursor-pointer py-2 group hover:bg-[#388d70] hover:rounded-md">
-      <div className=" w-2 rounded-sm bg-emerald-800 ml-1 mr-5 scale-0 group-hover:scale-100 delay-100 ease-in-out duration-300"></div>
-      <div className="text-emerald-800 group-hover:text-white">{curr.icon}</div>
-      <NavLink to={curr.link} className="text-emerald-800 w-20 group-hover:text-white">
+    <div
+      className={`hover:rounded-r-md flex flex-row gap-5 w-full mb-5 hover:cursor-pointer group hover:bg-emerald-600`}
+    >
+      <div className="w-2 bg-emerald-800 scale-0 group-hover:scale-100 delay-100 ease-in-out duration-300"></div>
+
+      <div className="group-hover:text-white">{curr.icon}</div>
+
+      <NavLink to={curr.link} className="w-20 group-hover:text-white">
         {curr.title}
       </NavLink>
     </div>
